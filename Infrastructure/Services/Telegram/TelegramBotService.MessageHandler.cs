c05426d90a752966d0eed0d6821268a113c85287
Trainian <Dimon007@inbox.ru>
2023-02-03 15:33:00 +0300

    отправкка запроса ПОСТ, при сообщении начинающегося с "1с".

--- Infrastructure/Services/Telegram/TelegramBotService.MessageHandler.cs
+++ Infrastructure/Services/Telegram/TelegramBotService.MessageHandler.cs
@@ -3,6 +3,8 @@ using Telegram.BotAPI;
 using Telegram.BotAPI.AvailableMethods;
 using Telegram.BotAPI.AvailableTypes;
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+using System.Threading;
 
 namespace Infrastructure.Services.Telegram
 {
@@ -25,6 +27,13 @@ namespace Infrastructure.Services.Telegram
             if(message?.Text?.StartsWith('/') == true) // Если это запрос действия бота
             {
 
+            }
+            else if(message?.Text?.ToLower().StartsWith("1с") == true)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    await PostAsJsonAsync(httpClient,message,Api);
+                }
             }
             else// Если это не запрос действия бота
             {
@@ -59,5 +68,38 @@ namespace Infrastructure.Services.Telegram
 
             await base.OnMessageAsync(message, cancellationToken);
         }
+
+        static async Task PostAsJsonAsync(HttpClient httpClient, Message message, BotClient api)
+        {
+            using HttpResponseMessage response = await httpClient.PostAsJsonAsync(
+                "http://topsite1.ru",
+                new { test = message.Text});
+
+            var result = response.EnsureSuccessStatusCode();
+
+            if(result.IsSuccessStatusCode)
+            {
+                await api.SendMessageAsync(message.Chat.Id,
+                    text: "Успешная отправка на ТОПСАЙТ",
+                    replyToMessageId: message.MessageId);
+                var answer = await response.Content.ReadAsStringAsync();
+                await api.SendMessageAsync(message.Chat.Id,
+                    text: $"Ответ: {answer}",
+                    replyToMessageId: message.MessageId);
+            }
+            else
+            {
+                await api.SendMessageAsync(message.Chat.Id,
+                    text: "Ошибка отправки запроса на ТОПСАЙТ",
+                    replyToMessageId: message.MessageId);
+            }
+
+            //var todo = await response.Content.ReadFromJsonAsync<Todo>();
+            //WriteLine($"{todo}\n");
+            // Expected output:
+            //   POST https://jsonplaceholder.typicode.com/todos HTTP/1.1
+            //   Todo { UserId = 9, Id = 201, Title = Show extensions, Completed = False }
+        }
+
     }
 }
